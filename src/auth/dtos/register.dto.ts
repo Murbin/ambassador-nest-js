cc6926cf31533211294d215e09d12a2dc451d59d
murbin <ma2urbina@gmail.com>
2025-03-30 10:29:01 -0500

    create auth services

---
+++ src/auth/dtos/register.dto.ts
@@ -0,0 +1,6 @@
+// dto means Data Transfer Object
+export interface RegisterDto {
+  first_name: string;
+  last_name: string;
+  email: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('ajas3');
+  console.log('ajas4');
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
